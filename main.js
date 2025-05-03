654bbdff78b57b64087981a96b383b894727c731
Marcell Nagy <marcelldnagy@gmail.com>
2025-05-03 19:59:23 +0200

    Add template method demo

--- main.js
+++ main.js
@@ -1,3 +1,39 @@
 "use strict";
-var SS = false;
-console.log(SS);
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var BaseClass = /** @class */ (function () {
+    function BaseClass(name) {
+        this.name = name;
+        this.name = name;
+        this.print();
+    }
+    BaseClass.prototype.redo = function () {
+        this.print();
+    };
+    return BaseClass;
+}());
+var SubClass = /** @class */ (function (_super) {
+    __extends(SubClass, _super);
+    function SubClass() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    SubClass.prototype.print = function () {
+        console.log(this.name);
+    };
+    return SubClass;
+}(BaseClass));
+var myInstance = new SubClass("Eish");
+myInstance.redo();
--- main.ts
+++ main.ts
@@ -1,2 +1,21 @@
-const SS = false;
-console.log(SS);
+abstract class BaseClass {
+    constructor(protected name: string) {
+        this.name = name;
+        this.print();
+    }
+
+    abstract print (): void;
+
+    redo (): void {
+        this.print();
+    }
+}
+
+class SubClass extends BaseClass {
+    print (): void {
+        console.log(this.name);
+    }
+}
+
+const myInstance = new SubClass("Eish");
+myInstance.redo();
